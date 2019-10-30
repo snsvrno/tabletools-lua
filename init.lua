4fa77f8d1dcdbd4e1d0c05368ac9dd6fe322377b
snsvrno <snsvrno@tuta.io>
2019-10-30 15:35:08 -0600

    fix: fixes dependency loading issue
    fixes an issue where you require this library not in the root of
    your project, and it can't find the dependecies because it is looking
    in the root of your project, not the folder you put this library.
    
    adds code to break appart the require path `a.b.c.d.library` and pulls
    out the parent folders `a.b.c.d.` and adds it to the dependency when it
    loads it `a.b.c.d.dependency` so it is now in the right folder.

--- init.lua
+++ init.lua
@@ -14,12 +14,42 @@ local dependencies = {
 	{ name = "stringtools", url = "https://github.com/snsvrno/stringtools-lua", version = "1.*" }
 }
 
+-- needs to findout what the pre-path stuff you used when calling this library, so
+-- it knows what to put in front of the dependencies, so if you call `a.b.c.library` it 
+-- needs to extract the `a.b.c.` part so it can put it infront of the dependencies to 
+-- get all dependencies
+
+local prepath; do
+    -- the string the user used to `require` this library
+    local selfRequirePath = (...):gsub('%.init$', '')
+    -- a function to split a string by a "."
+    local splitter = function (inputstr)
+        -- https://stackoverflow.com/questions/1426954/split-string-in-lua
+        local t = {}; for str in string.gmatch(inputstr, "([^".. "." .."]+)") do table.insert(t, str) end
+        return t
+    end
+
+    -- finds out what section of the path the library name is
+    local parts = splitter(selfRequirePath)
+    local piece = 0; for j = 1,#parts do
+        if string.find(parts[j],tools._name) then
+            piece = j - 1; break
+    end end
+
+    -- creates the prestring
+    assert(piece >= 0,"error processing the library require path")
+    local pre = ""; for i = 1,piece do
+        pre = pre .. parts[i] .. "."
+    end
+    prepath = pre
+end
+
 for _, dep in pairs(dependencies) do
 	local name, url = dep.name, dep.url
 	-- lets try a couple different permutations of the name, that
 	-- we might be expecting.
 	local results = { }; for _, nameVariant in pairs {name, name .. "-love", name .. "-lua"} do
-		local status, module = pcall(require, nameVariant);
+		local status, module = pcall(require, prepath .. nameVariant);
 		if status then results[#results + 1] = module end
 	end
 	-- lets check the results
